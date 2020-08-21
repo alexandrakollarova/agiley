1b285a2791360e3609c666649d8c9dbfc0157155
Alexandra Kollarova <alexandrakollarova91@gmail.com>
2020-08-21 14:14:44 -0400

    debugging

--- src/index.js
+++ src/index.js
@@ -30,7 +30,11 @@ const wsLink = new WebSocketLink({
   }
 })
 
+wsLink.subscriptionClient.close(false)
 wsLink.subscriptionClient.connect()
+
+wsLink.subscriptionClient.maxConnectTimeGenerator.duration = () =>
+  wsLink.subscriptionClient.maxConnectTimeGenerator.max
 
 wsLink.subscriptionClient.on('connecting', () => {
   console.log('connecting')
@@ -51,9 +55,6 @@ wsLink.subscriptionClient.on('reconnected', () => {
 wsLink.subscriptionClient.on('disconnected', () => {
   console.log('disconnected')
 })
-
-wsLink.subscriptionClient.maxConnectTimeGenerator.duration = () =>
-  wsLink.subscriptionClient.maxConnectTimeGenerator.max
 
 const link = split(
   ({ query }) => {
