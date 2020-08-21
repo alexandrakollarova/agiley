5589e10df1dbb734797a9bc1a0a5fa6ad28e3f3b
Alexandra Kollarova <alexandrakollarova91@gmail.com>
2020-08-21 18:06:29 -0400

    debugging websockets

--- src/index.js
+++ src/index.js
@@ -22,8 +22,15 @@ const wsLink = new WebSocketLink({
   uri: `wss://${config.API_ENDPOINT}/graphql`,
   options: {
     reconnect: true,
-    timeout: 30000
+    timeout: 30000,
+    minTimeout: 30000,
+    inactivityTimeout: 40000
   }
+})
+
+wsLink.subscriptionClient.on('error', (callback, ctx) => {
+  console.log('callback =>', callback)
+  console.log('ctx =>', ctx)
 })
 
 wsLink.subscriptionClient.on('connecting', () => {
